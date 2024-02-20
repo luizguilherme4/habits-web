99235f568724070adad75f3e6ea05f9214758101
Luiz Guilherme <101076506+luizguilherme4@users.noreply.github.com>
2024-02-19 20:28:36 -0400

    feat: style when on focus

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -13,7 +13,7 @@ interface HabitProps {
 
 export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitProps) {
   const [completed, setCompleted] = useState(defaultCompleted)
-  
+
   const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0
 
   const dayAndMonth = dayjs(date).format('DD/MM');
@@ -26,7 +26,7 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitProps)
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx("w-10 h-10 border-2  rounded-lg transition-colors", {
+        className={clsx("w-10 h-10 border-2  rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-background", {
           'bg-zinc-900 border-zinc-800': completedPercentage === 0,
           'bg-violet-900 border-violet-500': completedPercentage > 0 && completedPercentage < 20,
           'bg-violet-800 border-violet-500': completedPercentage >= 20 && completedPercentage < 40,
@@ -43,7 +43,7 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitProps)
 
           <ProgressBar progress={completedPercentage} />
 
-          <HabitsList date={date} onCompletedChange={handleCompletedChanged}/>
+          <HabitsList date={date} onCompletedChange={handleCompletedChanged} />
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
